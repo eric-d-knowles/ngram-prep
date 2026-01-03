efbd8731186b335c86d825635a19b4bc270f7e33
eric-d-knowles <edk202@nyu>
2026-01-03 15:42:21 -0500

    Code update

--- src/ngramprep/__init__.py
+++ src/ngramprep/__init__.py
@@ -1,3 +1,6 @@
+import os
+os.environ.setdefault('PYENCHANT_LIBRARY_PATH', '/scratch/edk202/.conda/envs/chrono-text/lib/libenchant-2.so')
+
 """ngram-kit: N-gram data preparation and training toolkit."""
 
 __version__ = "0.1.0"
