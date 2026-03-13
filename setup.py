5960116267f5a1a95e4ec4b06f93f2c4dc36a856
Eric Knowles <edk202@nyu.edu>
2026-03-13 17:18:36 -0400

    Refactor extension sources in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 import shutil
 import sys
 from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
 from Cython.Build import cythonize
 
 
@@ -20,22 +19,16 @@ def check_compiler():
 
 check_compiler()
 
-
-# Define extensions
 extensions = [
     Extension(
         "ngramprep.ngram_filter.filters.core_cy",
-        sources=[
-            "src/ngramprep/ngram_filter/filters/core_cy.pyx",
-        ],
+        sources=["src/ngramprep/ngram_filter/filters/core_cy.pyx"],
         language="c++",
         extra_compile_args=["-O3", "-std=c++17"],
     ),
     Extension(
         "daviesprep.davies_filter.filters.core_cy",
-        sources=[
-            "src/daviesprep/davies_filter/filters/core_cy.pyx",
-        ],
+        sources=["src/daviesprep/davies_filter/filters/core_cy.pyx"],
         language="c++",
         extra_compile_args=["-O3", "-std=c++17"],
     ),
