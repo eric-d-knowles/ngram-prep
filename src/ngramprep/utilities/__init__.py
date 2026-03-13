ec5bdbaa5760fa3d61175bdad97c9205270e2400
Eric Knowles <edk202@nyu.edu>
2026-03-13 18:39:53 -0400

    Update __init__.py to exclude count_db_items
    Removed count_db_items from the exported utilities.

--- src/ngramprep/utilities/__init__.py
+++ src/ngramprep/utilities/__init__.py
@@ -3,15 +3,10 @@
 
 from .display import format_bytes, truncate_path_to_fit, format_banner
 from .progress import ProgressDisplay
-from .count_items import count_db_items
 
 __all__ = [
-    # Display formatting
     "format_bytes",
     "truncate_path_to_fit",
     "format_banner",
-    # Progress display
     "ProgressDisplay",
-    # Count items
-    "count_db_items",
 ]
